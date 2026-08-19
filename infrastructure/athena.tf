4c800cbc1e4a0798fc581ee1bd734d47c00468ed
Justin Hawkins <jhawkins33@gmail.com>
2026-08-19 16:44:52 -0600

    Add force_destroy to Athena workgroup to simplify teardown

--- infrastructure/athena.tf
+++ infrastructure/athena.tf
@@ -1,6 +1,6 @@
 resource "aws_athena_workgroup" "main" {
   name = "${var.project}-workgroup"
-
+  force_destroy = true
   configuration {
     result_configuration {
       output_location = "s3://${aws_s3_bucket.athena_results.bucket}/results/"
